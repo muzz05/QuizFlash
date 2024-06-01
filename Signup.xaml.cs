481e120002ab277f11c0443430b4c42519e2681c
MUHAMMAD KAIF <148961089+Kaifrizwan12@users.noreply.github.com>
2024-06-01 13:20:02 +0500

    signup half

--- Signup.xaml.cs
+++ Signup.xaml.cs
@@ -14,14 +14,88 @@ using System.Windows.Shapes;
 
 namespace QuizFlash
 {
-    /// <summary>
-    /// Interaction logic for Signup.xaml
-    /// </summary>
     public partial class Signup : Window
     {
         public Signup()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PasswordBox_signup_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(passwordBox_signup.Password) && passwordBox_signup.Password.Length > 0)
+                textPassword_signup.Visibility = Visibility.Collapsed;
+            else
+                textPassword_signup.Visibility = Visibility.Visible;
+        }
+
+        private void textPassword_signup_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            passwordBox_signup.Focus();
+        }
+
+        private void Signup_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUsername_signup.Text) && !string.IsNullOrEmpty(passwordBox_signup.Password))
+            {
+                MessageBox.Show("Successfully Signed In");
+            }
+        }
+
+        private void txtUsername_signup_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUsername_signup.Text) && txtUsername_signup.Text.Length > 0)
+                textUsername_signup.Visibility = Visibility.Collapsed;
+            else
+                textUsername_signup.Visibility = Visibility.Visible;
+        }
+
+        private void txtEmail_signup_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail_signup.Text) && txtEmail_signup.Text.Length > 0)
+                textEmail_signup.Visibility = Visibility.Collapsed;
+            else
+                textEmail_signup.Visibility = Visibility.Visible;
+        }
+
+        private void txtSecurity_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtSecurity.Text) && txtSecurity.Text.Length > 0)
+                textSecurity.Visibility = Visibility.Collapsed;
+            else
+                textSecurity.Visibility = Visibility.Visible;
+        }
+
+        private void textSecurity_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtSecurity.Focus();
+        }
+
+
+
+        private void textUsername_signup_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtUsername_signup.Focus();
+        }
+
+
+
+
+
+        private void textEmail_signup_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail_signup.Focus();
         }
     }
 }
