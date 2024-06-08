0c1db0c925db272bc7ad82d80a8b5607abf7fccc
Muzzammil Ahmed <142844170+muzz05@users.noreply.github.com>
2024-06-08 20:15:40 +0500

    FlashCard UI and Logical Flow Changed

---
+++ FlashCard.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuizFlash
+{
+    public partial class Flashcard : UserControl
+    {
+        public Flashcard(string Title, string Description)
+        {
+            InitializeComponent();
+            FlashCardTitle.Text = Title;
+            FlashCardDescription.Text = Description;
+        }
+
+        private void FlashCardTitleTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var textBlock = sender as TextBlock;
+            var textBox = textBlock?.Tag as TextBox;
+            if (textBox != null)
+            {
+                textBox.Visibility = Visibility.Visible;
+                textBox.Focus();
+            }
+        }
+
+        private void FlashCardTitleTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var textBlock = textBox?.Tag as TextBlock;
+            if (textBlock != null)
+            {
+                textBlock.Text = textBox.Text;
+                textBox.Visibility = string.IsNullOrEmpty(textBox.Text) ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+
+        private void FlashCardTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var textBlock = sender as TextBlock;
+            var textBox = textBlock?.Tag as TextBox;
+            if (textBox != null)
+            {
+                textBox.Visibility = Visibility.Visible;
+                textBox.Focus();
+            }
+        }
+
+        private void FlashCardTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var textBlock = textBox?.Tag as TextBlock;
+            if (textBlock != null)
+            {
+                textBlock.Text = textBox.Text;
+                textBox.Visibility = string.IsNullOrEmpty(textBox.Text) ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+    }
+}
