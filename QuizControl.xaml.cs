0c206dd76f98be6d00713336a878205fcd120166
Muzzammil Ahmed <142844170+muzz05@users.noreply.github.com>
2024-06-20 02:27:20 +0500

    Minor Changes and the Addition of a Utility class for commonly used functions

--- QuizControl.xaml.cs
+++ QuizControl.xaml.cs
@@ -16,9 +16,7 @@ using System.Windows.Shapes;
 
 namespace QuizFlash
 {
-    /// <summary>
-    /// Interaction logic for QuizControl.xaml
-    /// </summary>
+
     public partial class QuizControl : UserControl
     {
         public QuizControl(string quizname, int totalmarks, int questions, long validUntilEpoch, bool IsAttempted)
@@ -32,22 +30,18 @@ namespace QuizFlash
 
             // Convert epoch timestamp to ISO 8601 string (assuming validUntilEpoch is in seconds)
             string validUntilIsoString = ConvertEpochToIsoString(validUntilEpoch);
-            string validUntilDate = ConvertIsoStringToDate(validUntilIsoString);  // New line for date conversion
+            string validUntilDate = ConvertIsoStringToDate(validUntilIsoString); 
             ValidUntil.Text= validUntilDate;
         }
 
         private string ConvertEpochToIsoString(long epochTimestamp)
         {
-            // Assuming epoch time is in seconds (multiply by 1000 for milliseconds)
             var dateTime = DateTimeOffset.FromUnixTimeMilliseconds(epochTimestamp * 1000);
-            return dateTime.UtcDateTime.ToString("o"); // Use 'o' format for ISO 8601 with UTC time
+            return dateTime.UtcDateTime.ToString("o");
         }
         private string ConvertIsoStringToDate(string isoString)
         {
-            // Parse the ISO 8601 string
             DateTime parsedDateTime = DateTime.Parse(isoString);
-
-            // Use 'd' format specifier for day/month/year
             return parsedDateTime.ToString("d");  
         }
     }
