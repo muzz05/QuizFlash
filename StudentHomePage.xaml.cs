72771dd5ebe48bcc1bf82bf79705773172196dc1
MUHAMMAD KAIF <148961089+Kaifrizwan12@users.noreply.github.com>
2024-06-20 10:04:48 +0500

    home page info card

--- StudentHomePage.xaml.cs
+++ StudentHomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuizFlash;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,8 +22,34 @@ namespace QuizFlash
     public partial class StudentHomePage : Page
     {
         public StudentHomePage()
-        {
+        { 
             InitializeComponent();
+            var cards = new[]
+            {
+                    new StudentHomepageInfoCard("Software Engineering", "Grand Quiz on 7/9/24"),
+                    new StudentHomepageInfoCard("Applied Physics", "Chapter 1 Test on 9/6/24"),
+                    new StudentHomepageInfoCard("Mathematics", "Midterm Exam on 8/15/24"),
+                    new StudentHomepageInfoCard("History", "Presentation on 9/1/24"),
+                    new StudentHomepageInfoCard("Software Engineering", "Final Exam on 9/1/24")
+
+            };
+
+            foreach (var card in cards)
+            {
+                card.Margin = new Thickness(8);
+                infocards.Children.Add(card);
+            }
+
         }
     }
 }
+
+
+//private void AddClassroom(string coursename, string code, string teacher, int count, string gcr_code, int classroomId)
+//{
+
+//    Classroom newClassroom = new Classroom(coursename, code, teacher, count, gcr_code, false, classroomId);
+//    newClassroom.Margin = new Thickness(0, 0, 15, 15);
+//    WrapPanelClassroom.Children.Add(newClassroom);
+
+//}
