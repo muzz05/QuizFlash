26238f1b3323d2ef93cee9f01b6815ba0abb0835
mathwhore <syedhaidershahid391@gmail.com>
2024-06-27 01:42:05 +0500

    Quiz Design and Display backend done.

--- Utilities.cs
+++ Utilities.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace QuizFlash
 {
-    public static class Utilities
+    public class Utilities
     {
 
         // To Get the Initials of a name
@@ -30,6 +30,26 @@ namespace QuizFlash
             DateTime currentTime = DateTime.UtcNow;
             DateTimeOffset dateTimeOffset = new DateTimeOffset(currentTime);
             return dateTimeOffset.ToUnixTimeSeconds();
+        }
+
+        //Determining the grade of a student based on marks
+        public static string GetGrade(int marks)
+        {
+            switch (marks)
+            {
+                case int n when (n >= 90):
+                    return "A+";
+                case int n when (n >= 80):
+                    return "A";
+                case int n when (n >= 70):
+                    return "B";
+                case int n when (n >= 60):
+                    return "C";
+                case int n when (n >= 50):
+                    return "D";
+                default:
+                    return "F";
+            }
         }
 
         // To Convert Epoch to Relative time (just now, 2 hours ago etc.)
